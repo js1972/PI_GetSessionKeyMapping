3a1c1dbabfcc3391421aecdcb4f6e17cf9abe071
Jason Scott <jason@jaylin.com.au>
2014-07-31 13:08:24 +0800

    Remove the un-used communication channel class and interface as its been moved to the library project

--- src/au/com/inpex/mapping/CommunicationChannel.java
+++
@@ -1,8 +0,0 @@
-package au.com.inpex.mapping;
-
-import com.sap.aii.mapping.lookup.LookupException;
-import com.sap.aii.mapping.lookup.Payload;
-
-public interface CommunicationChannel {
-	public Payload call(Payload request) throws LookupException;
-}
